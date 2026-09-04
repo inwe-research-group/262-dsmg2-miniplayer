68488c8841be5bdfe0032b38c284f73f37a24440
inwe-research-group <zmamanir@unmsm.edu.pe>
2026-09-03 20:15:44 -0500

    modify MainActivity

--- app/src/main/java/com/dsm/miniplayerg2/MainActivity.kt
+++ app/src/main/java/com/dsm/miniplayerg2/MainActivity.kt
@@ -6,42 +6,35 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.dsm.miniplayerg2.navigation.NavigationWrapper
 import com.dsm.miniplayerg2.ui.theme.MiniplayerG2Theme
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val auth = Firebase.auth
         setContent {
+            val navHostController = rememberNavController()
             MiniplayerG2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    NavigationWrapper(navHostController, auth)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MiniplayerG2Theme {
-        Greeting("Android")
-    }
-}
