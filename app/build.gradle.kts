b0545f7947276635822da47fa52e9eefe9c9a231
inwe-research-group <zmamanir@unmsm.edu.pe>
2026-09-10 19:02:33 -0500

    conf dep firestore add add HomeViewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     // Firebase (BOM controla versiones)
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
 
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
